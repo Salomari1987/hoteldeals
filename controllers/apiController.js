c6c262116811414def394db00c984c81396483f9
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-03-08 01:52:36 +0200

    Refactor

---
+++ controllers/apiController.js
@@ -0,0 +1,9 @@
+var hotelsSvc = require('../services/hotelsSvc.js')
+
+module.exports = {
+	getAll: function (req, res) {
+	    hotelsSvc.getAll(function (err, resp, data){
+	      res.status(200).send(data)
+		})
+  	}
+}
--- services/hotelsSvc.js
+++ services/hotelsSvc.js
@@ -1,10 +1,8 @@
 var request = require('request')
-
 module.exports = {
-	getAll: function (options, cb) {
+	getAll: function (cb) {
 	     request.get("https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel", function (err, resp, body) {
 			body = JSON.parse(body)
-			console.log(2222)
 			cb(err, resp, body);
 	    })
 	}
