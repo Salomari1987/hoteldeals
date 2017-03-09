3f3100cfab3f950d6c7c19c0d464c597da638718
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-03-09 04:35:04 +0200

    Run server on production

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 var express = require('express');
-
 var app = express();
+var server = require('http').createServer(app);
+
 
 // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
+var PORT = process.env.PORT || 8000;
+
 // start listening to requests on port 8000
-var server = app.listen(8000, function () {
+app.listen(PORT, function () {
 	console.log("Listening on port 8000")
+});
+
+app.on ( 'uncaughtException', function () {
+  //Close connection
+  server.close();
+});
+
+// On kill
+app.on('SIGTERM', function() {
+  server.close();
+});
+
+//On exit
+app.on('exit', function() {
+  server.close();
 });
 
 module.exports = {
